e8cc1150c1224efad340e9c744e989873aee3a35
Aleksandar Cvetković <arcvetkovic@gmail.com>
2015-04-13 15:15:40 +0200

    Add createTeamsTable

--- src/htsquirrel/DatabaseManagement.java
+++ src/htsquirrel/DatabaseManagement.java
@@ -30,6 +30,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -59,6 +60,40 @@ public class DatabaseManagement {
             }
         }
         return tableExists;
+    }
+    
+    // create teams table
+    public static void createTeamsTable(Connection connection)
+            throws SQLException {
+        String sqlCode = "CREATE TABLE TEAMS (" +
+                "USER_ID INT," +
+                "LOGIN_NAME VARCHAR(255)," +
+                "SUPPORTER_TIER VARCHAR(255)," +
+                "TEAM_ID INT," +
+                "TEAM_NAME VARCHAR(255)," +
+                "SHORT_TEAM_NAME VARCHAR(255)," +
+                "FOUNDED_DATE TIMESTAMP," +
+                "PRIMARY_CLUB BOOLEAN," +
+                "ARENA_ID INT," +
+                "ARENA_NAME VARCHAR(255)," +
+                "LEAGUE_ID INT," +
+                "LEAGUE_NAME VARCHAR(255)," +
+                "REGION_ID INT," +
+                "REGION_NAME VARCHAR(255)," +
+                "COACH_ID INT," +
+                "LEAGUE_LEVEL_UNIT_ID INT," +
+                "LEAGUE_LEVEL_UNIT_NAME VARCHAR(255)," +
+                "LEAGUE_LEVEL INT," +
+                "FANCLUB_ID INT," +
+                "FANCLUB_NAME VARCHAR(255)," +
+                "FANCLUB_SIZE INT," +
+                "LOGO_URI VARCHAR(255)," +
+                "DRESS_URI VARCHAR(255)," +
+                "DRESS_ALTERNATE_URI VARCHAR(255)" +
+                ")";
+        Statement statement = connection.createStatement();
+        statement.execute(sqlCode);
+        statement.close();
     }
 
 }
