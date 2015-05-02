fc2f69bfba03328a25ef75fa04f5f7719fdbf7a6
Aleksandar Cvetković <arcvetkovic@gmail.com>
2015-05-02 17:50:40 +0200

    Get user id from Hattrick

---
+++ src/htsquirrel/oauth/Responses.java
@@ -0,0 +1,65 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Aleksandar Cvetković
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package htsquirrel.oauth;
+
+import static htsquirrel.oauth.OAuth.getResponse;
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.scribe.model.Token;
+import org.scribe.oauth.OAuthService;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Aleksandar Cvetković <arcvetkovic@gmail.com>
+ */
+public class Responses {
+    
+    public static int getUserIdFromHt(OAuthService oAuthService, Token accessToken)
+            throws ParserConfigurationException, SAXException, IOException {
+        int userId;
+        String xmlString = getResponse(oAuthService, accessToken, "teamdetails&version=3.2");
+        Document document = xmlStringToDoc(xmlString);
+        document.getDocumentElement().normalize();
+        Element userElement = (Element) document.getElementsByTagName("User").item(0);
+        userId = Integer.parseInt(userElement.getElementsByTagName("UserID").item(0).getTextContent());
+        return userId;
+    }
+    
+    public static Document xmlStringToDoc(String xmlString)
+            throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        InputSource inputSource = new InputSource(new StringReader(xmlString));
+        Document document = dBuilder.parse(inputSource);
+        return document;
+    }
+    
+}
