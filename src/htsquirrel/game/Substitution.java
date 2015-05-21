7db62a18792caf23fc705db9bd034e1b4fa0f4f9
Aleksandar Cvetković <arcvetkovic@gmail.com>
2015-05-21 22:27:10 +0200

    Add Substitution

---
+++ src/htsquirrel/game/Substitution.java
@@ -0,0 +1,117 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Aleksandar Cvetković
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package htsquirrel.game;
+
+/**
+ *
+ * @author aleksandar
+ */
+public class Substitution {
+    
+    private long matchId;
+    private int teamId;
+    private int subjectPlayerId;
+    private int objectPlayerId;
+    private int orderType;
+    private int newPosition;
+    private int role;
+    private int behaviour;
+    private int minute;
+
+    public Substitution() {
+    }
+
+    public long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(long matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public int getSubjectPlayerId() {
+        return subjectPlayerId;
+    }
+
+    public void setSubjectPlayerId(int subjectPlayerId) {
+        this.subjectPlayerId = subjectPlayerId;
+    }
+
+    public int getObjectPlayerId() {
+        return objectPlayerId;
+    }
+
+    public void setObjectPlayerId(int objectPlayerId) {
+        this.objectPlayerId = objectPlayerId;
+    }
+
+    public int getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(int orderType) {
+        this.orderType = orderType;
+    }
+
+    public int getNewPosition() {
+        return newPosition;
+    }
+
+    public void setNewPosition(int newPosition) {
+        this.newPosition = newPosition;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getBehaviour() {
+        return behaviour;
+    }
+
+    public void setBehaviour(int behaviour) {
+        this.behaviour = behaviour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+    
+}
